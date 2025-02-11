3d18fc6d8f42aaca4e29877b028d8467b6574907
heeso <khs@alpinelab.io>
2025-02-11 16:58:06 +0900

    🔥

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,7 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { createRoutesFromElements } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import RootLayout from '@/layout/RootLayout';
 import WordcloudEx from '@/pages/WordcloudEx';
 import RadialBarsEx from '@/pages/RadialBarsEx';
-import Example from '@/pages/Example';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -12,7 +9,6 @@ const router = createBrowserRouter(
       <Route path='/' element={<RootLayout />}>
         <Route path='wordcloud' element={<WordcloudEx />} />
         <Route path='radialbars' element={<RadialBarsEx/>} />
-        <Route path='examples' element={<Example width={1300} height={1300}/>} />
       </Route>
     </>
   )
