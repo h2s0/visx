b0427616a7db5702c7e5a50763a4004a6a8c1722
heeso <khs@alpinelab.io>
2025-02-20 17:14:35 +0900

    파일 이름 변경

---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+import { ESLintConfig } from '@eslint/eslintrc';
+
+const config: ESLintConfig = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
+
+export default config;
