6d21327060cd661e7f28998d8d177b0af11eebaf
heeso <h2s0_@naver.com>
2025-02-21 17:41:49 +0900

    팀 정보 보이는 페이지

--- src/pages/TeamInfo.tsx
+++ src/pages/TeamInfo.tsx
@@ -1,8 +1,27 @@
+import createTeamInfo from "@/data/createTeamInfo";
+import { useEffect, useState } from "react";
+
 const TeamInfo: React.FC = () => {
+  const [ teams, setTeams ] = useState([]);
+
+  useEffect(() => {
+    const newTeams = createTeamInfo(20);
+    setTeams(newTeams);
+  }, []);
 
   return(
-    <>
-    </>
+    <section className="flex gap-5 w-full flex-wrap">
+      {teams.map((team, i) => (
+        <div key={i} className="border p-3">
+          <p>팀 이름: {team.name}</p>
+          <p>점수: {team.score}</p>
+          <p>나라: {team.country}</p>
+          <p>색깔: {team.color}</p>
+          <div></div>
+        </div>
+      ))}
+      
+    </section>
   )
 };
 
