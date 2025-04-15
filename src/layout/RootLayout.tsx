bdcfe306ed01a8ec16df6397be079c90600aa5ee
heeso <h2s0_@naver.com>
2025-04-15 13:28:14 +0900

    ✨ ♻️ 화면 전환 시 애니메이션 추가 및 코드 구조 변경

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -3,18 +3,16 @@ import RadialBarsEx from "@/pages/RadialBarsEx";
 import WordcloudEx from "@/pages/WordcloudEx";
 import { useEffect, useState } from "react";
 import { Outlet } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const RootLayout: React.FC = () => {
-  const [currentComponent, setCurrentComponent] = useState<JSX.Element | null>(<WordcloudEx />);
+  // 표시 할 컴포넌트 목록
+  const components = [ <WordcloudEx />, <RadialBarsEx />, <FloatingCards /> ];
+  const [ index, setIndex ] = useState(0);
 
   useEffect(() => {
-    // 표시할 컴포넌트 목록
-    const components = [ <WordcloudEx />, <RadialBarsEx />, <FloatingCards /> ];
-    let index = 0;
-
     const interval = setInterval(() => {
-      index = (index + 1) % components.length;
-      setCurrentComponent(components[index]);
+      setIndex((prev) => (prev + 1) % components.length);
     }, 10000); // 10초마다 변경
 
     return () => clearInterval(interval);
@@ -22,7 +20,15 @@ const RootLayout: React.FC = () => {
 
   return (
     <main className='flex flex-col text-white'>
-      {currentComponent}
+      <motion.div
+        key={index}
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+      >
+        {components[index]}
+      </motion.div>
       <Outlet />
     </main>
   )
