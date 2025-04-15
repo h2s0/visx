7afc1a28f8a9458e858ba5b238b0d58164109a01
heeso <h2s0_@naver.com>
2025-04-15 13:22:23 +0900

    🎨 10초마다 화면 바뀌게 설정

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,16 +1,28 @@
-import { Link } from "react-router-dom";
+import FloatingCards from "@/pages/FloatingCards";
+import RadialBarsEx from "@/pages/RadialBarsEx";
+import WordcloudEx from "@/pages/WordcloudEx";
+import { useEffect, useState } from "react";
 import { Outlet } from 'react-router-dom';
 
 const RootLayout: React.FC = () => {
+  const [currentComponent, setCurrentComponent] = useState<JSX.Element | null>(<WordcloudEx />);
+
+  useEffect(() => {
+    // 표시할 컴포넌트 목록
+    const components = [ <WordcloudEx />, <RadialBarsEx />, <FloatingCards /> ];
+    let index = 0;
+
+    const interval = setInterval(() => {
+      index = (index + 1) % components.length;
+      setCurrentComponent(components[index]);
+    }, 10000); // 10초마다 변경
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <main className='flex flex-col text-white'>
-      <div className="flex gap-5 p-5 font-bold">
-        <Link to='wordcloud'>Wordcloud</Link>
-        <Link to='radialbars'>Radial Bars</Link>
-        <Link to='floatingcards'>Floating Cards</Link>
-        <Link to='display'>Display</Link>
-        <Link to='teamInfo'>Team Information</Link>
-      </div>
+      {currentComponent}
       <Outlet />
     </main>
   )
