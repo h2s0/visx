ac90aa043761dc5e87d1c30aa5c2670d62d4a66d
heeso <h2s0_@naver.com>
2025-07-04 12:10:26 +0900

    ✨ 이모지 국기 함수

---
+++ src/utils/countryCodeToFlagEmoji.ts
@@ -0,0 +1,7 @@
+export function countryCodeToFlagEmoji(code: string) {
+  return code
+    .toUpperCase()
+    .replace(/./g, char =>
+      String.fromCodePoint(127397 + char.charCodeAt(0))
+    );
+}
